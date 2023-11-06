8960bde7c0fab2b8cbecfbde13ce2e640bd4291e
Vladislav <31704441+MedievalRain@users.noreply.github.com>
2023-11-07 02:19:13 +0300

    Logo

---
+++ src/ui/Logo/Logo.tsx
@@ -0,0 +1,9 @@
+import { memo } from "react"
+import styles from './Logo.module.css'
+const Logo=memo(function Logo() {
+    return (
+        <h1 className={styles.logo}>Sækja</h1>
+    )
+})
+
+export default Logo
