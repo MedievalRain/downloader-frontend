203e78671b7d481f4eba388402aa4aaf66a7f76c
Vladislav <31704441+MedievalRain@users.noreply.github.com>
2023-11-07 02:45:17 +0300

    Youtube badge

---
+++ src/ui/badges/YoutubeBadge/YoutubeBadge.tsx
@@ -0,0 +1,11 @@
+import { memo } from "react"
+import styles from './YoutubeBadge.module.css'
+const YoutubeBadge=memo(function YoutubeBadge() {
+    return (
+        <span className={styles.badge}>
+            YouTube
+        </span>
+    )
+})
+
+export default YoutubeBadge
