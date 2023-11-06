187cb3e89b16f840c8c9730e050040b763aa9783
Vladislav <31704441+MedievalRain@users.noreply.github.com>
2023-11-07 02:19:21 +0300

    App layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import './App.css'
-
+import Input from './ui/Input/Input'
+import styles from './App.module.css'
+import Logo from './ui/Logo/Logo'
 function App() {
 return (
-    <>
-    </>
+    <div className={styles.container}><Logo/><Input placeholder='Paste any YouTube link here'/></div>
   )
 }
 
