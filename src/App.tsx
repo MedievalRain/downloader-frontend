feccb182cabf79a7cd26165517209836f0f6ea61
Vladislav <31704441+MedievalRain@users.noreply.github.com>
2023-11-07 02:45:57 +0300

    Adjust layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
-import Input from './ui/Input/Input'
-import styles from './App.module.css'
-import Logo from './ui/Logo/Logo'
+import Input from "./ui/Input/Input";
+import styles from "./App.module.css";
+import Logo from "./ui/Logo/Logo";
+import Description from "./ui/Description/Description";
 function App() {
-return (
-    <div className={styles.container}><Logo/><Input placeholder='Paste any YouTube link here'/></div>
-  )
+  return (
+    <div className={styles.container}>
+      <Logo />
+      <Description />
+      <Input placeholder="Paste any YouTube link here" />
+    </div>
+  );
 }
 
-export default App
+export default App;
