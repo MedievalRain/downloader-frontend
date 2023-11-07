0bceb710a0f359b00f51dbeb022c95552ed50acd
Vladislav <31704441+MedievalRain@users.noreply.github.com>
2023-11-07 05:28:08 +0300

    Add badges

--- src/ui/Description/Description.tsx
+++ src/ui/Description/Description.tsx
@@ -2,11 +2,13 @@ import { memo } from "react";
 import styles from "./Description.module.css";
 import YoutubeBadge from "../badges/YoutubeBadge/YoutubeBadge";
 import XBadge from "../badges/XBadge/XBadge";
+import RedditBadge from "../badges/RedditBadge/RedditBadge";
+import TiktokBadge from "../badges/TiktokBadge/TiktokBadge";
 
 const Description = memo(function Description() {
   return (
     <p className={styles.description}>
-      Download any video from <YoutubeBadge />, <XBadge />
+      Download any video from <YoutubeBadge />, <XBadge />, <RedditBadge /> and <TiktokBadge />
     </p>
   );
 });
