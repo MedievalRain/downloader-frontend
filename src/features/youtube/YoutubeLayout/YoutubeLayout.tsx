2f02fce4d500d9bf08e0cad898680ec3c3a28dd3
Vladislav <31704441+MedievalRain@users.noreply.github.com>
2023-11-07 18:49:31 +0300

    Embed video

--- src/features/youtube/YoutubeLayout/YoutubeLayout.tsx
+++ src/features/youtube/YoutubeLayout/YoutubeLayout.tsx
@@ -1,30 +1,45 @@
 import { Dispatch, SetStateAction, useEffect, useState } from "react";
 import { VideoInfo } from "../types";
-
+import styles from "./YoutubeLayout.module.css";
 interface YoutubeLayoutProps {
   url: string;
   setIsLoading: Dispatch<SetStateAction<boolean>>;
+  isLoading: boolean;
 }
 
-function YoutubeLayout({ url, setIsLoading }: YoutubeLayoutProps) {
+function YoutubeLayout({ url, isLoading, setIsLoading }: YoutubeLayoutProps) {
   const [videoInfo, setVideoInfo] = useState<VideoInfo>();
 
   useEffect(() => {
-    const fetchVideoInfo = async () => {
-      const response = await fetch(`${import.meta.env.VITE_BASE_API_URL}/youtube/info?url=${url}`);
-      if (response.ok) {
-        const data = (await response.json()) as VideoInfo;
-        setVideoInfo(data);
-      } else {
-        console.error(response.url);
-      }
-      setIsLoading(false);
-    };
+    if (isLoading) {
+      const fetchVideoInfo = async () => {
+        const response = await fetch(`${import.meta.env.VITE_BASE_API_URL}/youtube/info?url=${url}`);
+        if (response.ok) {
+          const data = (await response.json()) as VideoInfo;
+          setVideoInfo(data);
+        } else {
+          console.error(response.url);
+        }
+        setIsLoading(false);
+      };
+      fetchVideoInfo();
+    }
+  }, [url, isLoading]);
 
-    fetchVideoInfo();
-  }, [url]);
-
-  return <div>{videoInfo?.id}</div>;
+  return (
+    <div className={styles.container}>
+      {videoInfo ? (
+        <div className="video-responsive">
+          <iframe
+            className={styles.video}
+            src={`https://www.youtube.com/embed/${videoInfo.id}`}
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          />
+        </div>
+      ) : null}
+    </div>
+  );
 }
 
 export default YoutubeLayout;
