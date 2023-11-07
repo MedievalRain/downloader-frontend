979f865dcc1bf90af5678f22d893c93e4a07d7bc
Vladislav <31704441+MedievalRain@users.noreply.github.com>
2023-11-07 16:29:59 +0300

    Change CSS variable directly

--- src/features/url/UrlControls/UrlControls.tsx
+++ src/features/url/UrlControls/UrlControls.tsx
@@ -1,26 +1,29 @@
-import { CSSProperties, ChangeEvent, memo, useEffect, useState } from "react";
+import { ChangeEvent, memo, useEffect, useState } from "react";
 import Input from "../../../ui/Input/Input";
 import { Source } from "../../../types";
 import styles from "./UrlControls.module.css";
 import { getColorFromSource, getSourceFromUrl } from "../utils";
-import SearchButton from "../../SearchButton/SearchButton";
+import SearchButton from "../SearchButton/SearchButton";
 const UrlControls = memo(function UrlControls() {
   const [url, setUrl] = useState("");
   const [source, setSource] = useState<Source | null>("youtube");
   useEffect(() => {
     setSource(getSourceFromUrl(url));
   }, [url]);
-  const sourceColor = { "--source-color": getColorFromSource(source) } as CSSProperties;
+  useEffect(() => {
+    const color = getColorFromSource(source);
+    document.documentElement.style.setProperty("--source-color", color);
+  }, [source]);
+
   return (
     <div className={styles.container}>
       <Input
-        style={sourceColor}
         placeholder="Paste video link here"
         className={source ? styles[source] : ""}
         value={url}
         onChange={(e: ChangeEvent<HTMLInputElement>) => setUrl(e.target.value)}
       />
-      <SearchButton sourceColor={sourceColor} />
+      <SearchButton />
     </div>
   );
 });
