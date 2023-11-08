cfadd303a6325c82d3038da4afd6e872bc13fb37
Vladislav <31704441+MedievalRain@users.noreply.github.com>
2023-11-08 14:43:16 +0300

    Remove bages for not implemented sources

--- src/ui/Description/Description.tsx
+++ src/ui/Description/Description.tsx
@@ -1,14 +1,11 @@
 import { memo } from "react";
 import styles from "./Description.module.css";
 import YoutubeBadge from "../badges/YoutubeBadge/YoutubeBadge";
-import XBadge from "../badges/XBadge/XBadge";
-import RedditBadge from "../badges/RedditBadge/RedditBadge";
-import TiktokBadge from "../badges/TiktokBadge/TiktokBadge";
 
 const Description = memo(function Description() {
   return (
     <p className={styles.description}>
-      Download any video from <YoutubeBadge />, <XBadge />, <RedditBadge /> and <TiktokBadge />
+      Download any video from <YoutubeBadge />
     </p>
   );
 });
