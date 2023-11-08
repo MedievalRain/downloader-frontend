607a7ffe7466e179021eab0d0d8eacda8e06145b
Vladislav <31704441+MedievalRain@users.noreply.github.com>
2023-11-09 00:27:34 +0300

    Remove unused types

--- src/features/search/utils.ts
+++ src/features/search/utils.ts
@@ -5,13 +5,6 @@ export const getSourceFromUrl = (url: string): Source | null => {
   switch (domain) {
     case "youtube.com":
       return "youtube";
-    case "twitter.com":
-    case "x.com":
-      return "x";
-    case "reddit.com":
-      return "reddit";
-    case "tiktok.com":
-      return "tiktok";
     default:
       return null;
   }
@@ -21,12 +14,12 @@ export const getColorFromSource = (source: Source | null): string => {
   switch (source) {
     case "youtube":
       return "#dc2626";
-    case "x":
-    case "tiktok":
-      return "#000000";
-    case "reddit":
-      return "#ff4500";
     default:
       return "#1a202c";
   }
 };
+// case "x":
+//   case "tiktok":
+//     return "#000000";
+//   case "reddit":
+//     return "#ff4500";
