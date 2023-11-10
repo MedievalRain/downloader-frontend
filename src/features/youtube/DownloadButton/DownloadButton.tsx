60c4bb66cbd32a41929b7d1c09ff9e93a01f50c8
Vladislav <31704441+MedievalRain@users.noreply.github.com>
2023-11-10 04:21:47 +0300

    Download via single request

--- src/features/youtube/DownloadButton/DownloadButton.tsx
+++ src/features/youtube/DownloadButton/DownloadButton.tsx
@@ -13,16 +13,26 @@ const DownloadButton = memo(function DownloadButton({ pickedAudio, pickedVideo, 
 
   const downloadVideo = async () => {
     setIsLoading(true);
-
-    const params = { id, video: pickedVideo || "", audio: pickedAudio || "" };
-    const response = await fetch(`${import.meta.env.VITE_BASE_API_URL}/youtube/url?${new URLSearchParams(params)}`);
-    if (response.ok) {
-      const data = (await response.json()) as { filename: string };
-      window.location.href = `${import.meta.env.VITE_BASE_API_URL}/youtube/download/${data.filename}?videoname=${title}`;
-    } else {
-      console.error(response.url);
+    try {
+      const params = { id, title, video: pickedVideo || "", audio: pickedAudio || "" };
+      const response = await fetch(`${import.meta.env.VITE_BASE_API_URL}/youtube/download?${new URLSearchParams(params)}`);
+      if (!response.ok) {
+        throw new Error(`Server responded with ${response.status}`);
+      }
+      const blob = await response.blob();
+      const url = window.URL.createObjectURL(blob);
+      const a = document.createElement("a");
+      a.href = url;
+      a.download = title;
+      document.body.appendChild(a);
+      a.click();
+      window.URL.revokeObjectURL(url);
+      document.body.removeChild(a);
+    } catch (error) {
+      console.error("Error downloading file:", error);
+    } finally {
+      setIsLoading(false);
     }
-    setIsLoading(false);
   };
   return (
     <div className={styles["button-container"]}>
